63a599ee933a826be1bd76f4ec9d0c79f5bf7cc5
Дарья Токарева <gr413_toddm@lab-407-5.tomtit-tomsk.ru>
2023-01-30 10:35:49 +0700

    сделала первые три практические работы забыла пушить до этого извиняюсь хе хе хе

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks",
+        image: "./assets/vmSocks-blue-onWhite (1).jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 100,
+        onSale: true,
+    }
+})
+
+
+ 
+ 
