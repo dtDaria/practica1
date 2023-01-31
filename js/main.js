03f81dba6016f540acbd2a859a73c5134d5a61e0
gr413_toddm <38tosha@gmail.com>
2023-01-31 12:48:50 +0700

    не доделала

--- js/main.js
+++ js/main.js
@@ -1,3 +1,62 @@
+Vue.component('product-review', {
+    template: `
+
+   <form class="review-form" @submit.prevent="onSubmit">
+ <p>
+   <label for="name">Name:</label>
+   <input id="name" v-model="name" placeholder="name">
+ </p>
+
+ <p>
+   <label for="review">Review:</label>
+   <textarea id="review" v-model="review"></textarea>
+ </p>
+
+ <p>
+   <label for="rating">Rating:</label>
+   <select id="rating" v-model.number="rating">
+     <option>5</option>
+     <option>4</option>
+     <option>3</option>
+     <option>2</option>
+     <option>1</option>
+   </select>
+ </p>
+
+ <p>
+   <input type="submit" value="Submit"> 
+ </p>
+
+</form>
+ `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null
+        }
+    },
+    methods:{
+        onSubmit() {
+            let productReview = {
+                name: this.name,
+                review: this.review,
+                rating: this.rating,
+                reviews: []
+            }
+            this.$emit('review-submitted', productReview)
+            this.name = null
+            this.review = null
+            this.rating = null
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
+        }
+    }
+
+})
+
+
 Vue.component('product-details',{
     props: {
         details: {
@@ -139,7 +198,8 @@ let app = new Vue({
                 }
             }
         }
-    }
+    },
+
 
 })
 
